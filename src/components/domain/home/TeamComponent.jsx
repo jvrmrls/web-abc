31443618db181720bf34df1306d4c86f4e43b4a8
JavierMorales0 <javiermoralesmelara@gmail.com>
2024-02-17 22:40:27 -0600

    fix: Responsive Desktop

--- src/components/domain/home/TeamComponent.jsx
+++ src/components/domain/home/TeamComponent.jsx
@@ -5,7 +5,7 @@ function TeamComponent() {
     <Box
       sx={style.container}
       id={"Equipo Creativo"}
-      className='flex flex-col justify-between  items-center py-6 lg:py-10  min-h-[220px] w-full lg:min-h-[750px]'
+      className='flex flex-col justify-around lg:justify-center lg:gap-12  items-center py-6 lg:py-10  min-h-[220px] w-full lg:min-h-[650px]'
     >
       <p className='text-2xl font-bold text-center leading-3 lg:text-6xl lg:leading-10'>
         Equipo{' '}
@@ -16,7 +16,6 @@ function TeamComponent() {
           creativo
         </span>
       </p>
-      <Box></Box>
       <Box className={"w-3/4 min-w-[250px] mx-auto"}>
         <p className={"text-xs text-center leading-3 lg:text-lg"}>
           Contamos con <span className='font-bold'>personal capacitado</span> para <br />
